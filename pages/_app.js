0bb499316481acd14f2244fd272dfd5b9c549a69
curyPD <romandruzhinin2003@gmail.com>
2023-01-31 14:27:40 +0300

    Optimize the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
+}
+export function reportWebVitals(metric) {
+    console.log(metric);
 }
